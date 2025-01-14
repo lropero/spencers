358847eeaa1b97abc35603d743a363fec149570d
Luciano Ropero <lysoform@mac.lan>
2025-01-14 13:14:02 -0300

    added sign

---
+++ src/components/sign.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Sign = () => {
+  const hover = {
+    color: '#333'
+  }
+
+  const style = {
+    backgroundColor: '#ff6347',
+    borderRadius: '0 0 10px 0',
+    boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+    color: '#f3f3f3',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    left: 0,
+    margin: 0,
+    padding: '12px 18px',
+    position: 'fixed',
+    textDecoration: 'none',
+    top: 0,
+    zIndex: 10000
+  }
+
+  return (
+    <a href='https://www.vocal0.com' onMouseEnter={event => (event.target.style.color = hover.color)} onMouseLeave={event => (event.target.style.color = style.color)} style={style}>
+      DEMO Vocal0
+    </a>
+  )
+}
+
+export default Sign
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
+import Sign from './components/sign'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Sign />
     <App />
   </BrowserRouter>
 )
